fbf8d12bf0e63e4c4e95860626f02ee6d7154574
Soebb <87156166+Soebb@users.noreply.github.com>
2022-02-11 21:55:32 +0330

    Update helper.py

--- helper.py
+++ helper.py
@@ -126,34 +126,27 @@ movie_tile_content = '''
 '''
 
 
-def create_movie_tiles_content(movies):
-    # The HTML content for this section of the page
-    content = ''
-    for movie in movies:
-        # Extract the youtube ID from the url
-        youtube_id_match = re.search(
-            r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
-        youtube_id_match = youtube_id_match or re.search(
-            r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
-        trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
-                              else None)
+def create_movie_tiles_content(yt_url, title):
+    # Extract the youtube ID from the url
+    youtube_id_match = re.search(
+        r'(?<=v=)[^&#]+', yt_url)
+    youtube_id_match = youtube_id_match or re.search(
+        r'(?<=be/)[^&#]+', yt_url)
+    trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
+                          else None)
 
-        # Append the tile for the movie with its content filled in
-        content += movie_tile_content.format(
-            movie_title=movie.title,
-            poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
-        )
-    return content
-
-
-def open_movies_page(movies):
+    # Append the tile for the movie with its content filled in
+    content = movie_tile_content.format(
+        movie_title=title,
+        poster_image_url=default_thumb,
+        trailer_youtube_id=trailer_youtube_id
+    )
     # Create or overwrite the output file
-    output_file = open('fresh_tomatoes.html', 'w')
+    output_file = open(title+'.html', 'w')
 
     # Replace the movie tiles placeholder generated content
     rendered_content = main_page_content.format(
-        movie_tiles=create_movie_tiles_content(movies))
+        movie_tiles=content)
 
     # Output the file
     output_file.write(main_page_head + rendered_content)
