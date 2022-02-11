c7cf3f0b49ae78af8331d6cd08476425c2d5c8f1
Soebb <87156166+Soebb@users.noreply.github.com>
2022-02-11 22:30:25 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+from pyromod import listen
+from stream import create_stream_url
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
+
+Bot = Client(
+    "Own-ST-Bot",
+    bot_token = BOT_TOKEN,
+    api_id = API_ID,
+    api_hash = API_HASH
+)
+
+START_TXT = """
+Hi {},
+I'm Own YouTube Stream URL Generator Bot.
+
+Send a YouTube URL to get started.
+"""
+
+START_BTN = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Source Code', url='https://github.com/samadii/WebDownloaderBot'),
+        ]]
+    )
+
+
+@Bot.on_message(filters.command(["start"]))
+async def start(bot, update):
+    text = START_TXT.format(update.from_user.mention)
+    reply_markup = START_BTN
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
+
+
+@Bot.on_message(filters.private & filters.text)
+async def gen_st_url(_, m):
+    yt_url = m.text
+    title =
+    own_url = create_stream_url(yt_url, title.text)
+    await m.reply(own_url)
+    #import
+
+
+Bot.run()
